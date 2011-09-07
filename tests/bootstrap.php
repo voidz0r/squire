79fa16ab88ec55a96e269e5d8facd68260b9959e
Alessandro Desantis <desa.alessandro@gmail.com>
2011-09-07 20:16:09 +0200

    Use of `ClassLoader` component to handle framework autoloading.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,18 +10,3 @@
  */
 
 require_once dirname(__FILE__) . '/../src/autoload.php';
-
-spl_autoload_register(function($class) {
-	$class = ltrim($class, '\\');
-	
-	if (strpos($class, 'Squire\\Tests') !== 0) {
-		return;
-	}
-	
-	$fname = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-	$fpath = dirname(__FILE__) . DIRECTORY_SEPARATOR . $fname;
-	
-	if (is_file($fpath)) {
-		require_once $path;
-	}
-}, true);
